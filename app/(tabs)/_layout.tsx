fed387d07e1c4da5ad587800283d1d0f35339d0a
Ermias Tefera <ermiastefera@21s-MacBook-Pro.local>
2025-01-03 11:18:00 +0300

    feat: add nativewind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="playlist"
         options={{
-          title: 'Explore',
+          title: 'Playlist',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
