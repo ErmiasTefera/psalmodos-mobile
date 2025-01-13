5a2e4f40d5c0f668d0972ba2e995d2613e61c2f5
Ermias Tefera <ermiastefera@21s-MacBook-Pro.local>
2025-01-13 13:17:43 +0300

    feat: category with mock data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,54 @@
-import { Text } from 'react-native';
+import React from "react";
+import { Text, View, FlatList } from "react-native";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { EllipsisVertical } from 'lucide-react-native';
+
 
 export default function HomeScreen() {
+  const [categories, setCategories] = React.useState([
+    { name: "ልደት", key: "lidet", numberOfSongs: 10, lastUpdated: "2021-09-11" },
+    {
+      name: "ጥምቀት",
+      key: "timket",
+      numberOfSongs: 10,
+      lastUpdated: "2021-09-11",
+    },
+  ]);
+
   return (
-    <Text className='m-10'>Home</Text>
+    <View className="mt-8 p-4 pt-8 flex gap-4 bg-d h-full">
+      <View className="flex flex-row gap-4">
+        <View className="flex-1">
+          <FlatList
+            data={categories}
+            renderItem={({ item }) => (
+              <Card className="flex-1 mt-2">
+                <CardHeader className="flex flex-row justify-between">
+                  <View>
+                  <CardTitle className="text-xl">{item.name}</CardTitle>
+                  <CardDescription>
+                    <Text>{item.numberOfSongs} መዝሙሮች</Text>
+                  </CardDescription>
+                  </View>
+                  <EllipsisVertical />
+                </CardHeader>
+                <CardContent>
+                  <Text>Last updated</Text>
+                  <CardDescription>
+                    <Text>{item.lastUpdated}</Text>
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            )}
+          />
+        </View>
+      </View>
+    </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { PortalHost } from "@rn-primitives/portal";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -35,6 +36,7 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
   );
 }
